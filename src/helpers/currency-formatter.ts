23e386661b4f60d71412f1b3d13aa487727d8875
jrivas <josuerivas2311@gmail.com>
2025-06-01 19:30:58 -0600

    Create currency format function

---
+++ src/helpers/currency-formatter.ts
@@ -0,0 +1,8 @@
+export class CurrencyFormatter {
+  static format(value: number): string {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(value);
+  }
+}
